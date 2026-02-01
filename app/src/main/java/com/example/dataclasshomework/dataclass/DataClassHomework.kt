93e785985b4d1e78df55acaf7c9fadeac169ce02
Victoria Zabelina <128737095+victo-zabel@users.noreply.github.com>
2026-02-01 11:25:50 +0300

    Update DataClassHomework.kt

--- app/src/main/java/com/example/dataclasshomework/dataclass/DataClassHomework.kt
+++ app/src/main/java/com/example/dataclasshomework/dataclass/DataClassHomework.kt
@@ -1,18 +1,5 @@
 package com.example.dataclasshomework.dataclass
 
-/**
- * ДЗ:
- * В уроке про дата классы мы рассматривали, как можно вывести в терминал имя первого учителя из списка.
- * Давайте чуть усложним задачу и напишем фильтрацию преподавателей по опыту.
- *
- * У нас есть список учителей, нужно вывести список имен учителей, опыт которых больше 5 лет.
- * А если такого учителя нет, то нужно вывести текст: "Учитель с таким опытом не найден"
- *
- * Для выполнения задачи понадобится методы filter и map из Kotlin.
- * Примеры использования можно изучить в официальной документации Kotlin:
- * 1. https://kotlinlang.ru/docs/collection-elements.html
- * 2. https://kotlinlang.ru/docs/map-operations.html
- */
 fun main() {
 
     val languageList = listOf(
@@ -27,12 +14,12 @@ fun main() {
         Teacher("Евгений", 2, 1, languageList)
     )
 
-    val experiencedTeacher = getExperiencedTeacher(experience = 5, teacherList = teacherList)
+    val experiencedTeacher = getExperiencedTeacher(experience = 11, teacherList = teacherList)
 
     if (experiencedTeacher.isNotEmpty()) {
         println(experiencedTeacher)
     } else {
-        // TODO если список имен пустой, то выведи в терминал "Учитель с таким опытом не найден"
+        println("Учитель с таким опытом не найден")
     }
 }
 
@@ -40,6 +27,7 @@ fun main() {
  * Метод поиска опытного учителя из списка
  */
 private fun getExperiencedTeacher(experience: Int, teacherList: List<Teacher>): List<String> {
-    // TODO сейчас метод возвращает пустой список. Напиши здесь фильтрацию поиска учителя из списка teacherList
-    return emptyList()
+    return teacherList.filter { it.progExperience > experience}.map {
+        it.name
+    }
 }
